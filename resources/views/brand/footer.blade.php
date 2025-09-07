9126c38bebf55535bf60bb1f9140482915818eea
rgarestuk <ranggarestukusuma20@gmail.com>
2025-09-07 14:19:42 +0700

    commit awal

---
+++ resources/views/brand/footer.blade.php
@@ -0,0 +1,10 @@
+<p class="small text-center m-n">
+    © Copyright {{date('Y')}} 
+    <a href="{{ config('app.url') }}" target="_blank">
+        {{ config('app.name') }}
+    </a>
+    <p class="inline-block text text-center">
+        Developed by <a href="github.com/rgarestuk" target="_blank">Rangga Restu Kusuma</a>
+    </p>
+    
+</p>
---
+++ resources/views/brand/header.blade.php
@@ -0,0 +1,13 @@
+@push('head')
+    
+@endpush
+
+<div class="h2 d-flex align-items-center">
+    @auth
+        <x-orchid-icon path="bs.house" class="d-inline d-xl-none"/>
+    @endauth
+
+    <p class="my-0 {{ auth()->check() ? 'd-none d-xl-block' : '' }}">
+        {{ config('app.name') }}
+    </p>
+</div>
